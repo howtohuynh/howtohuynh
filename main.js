97e5a5ed31d153bcd56249dab75bbdc8de4eaa38
Kenny <kennyhuynh2019@gmail.com>
2017-07-31 23:24:08 -0700

    First commit

---
+++ main.js
@@ -0,0 +1,40 @@
+$(document).ready(function() {
+ 
+  $('a.panel').click(function() {
+ 
+    $('a.panel').removeClass('selected');
+    $(this).addClass('selected');
+ 
+    current = $(this);
+ 
+    $('#wrapper').scrollTo($(this).attr('href'), 800);
+ 
+    return false;
+  });
+ 
+  $(window).resize(function() {
+    resizePanel();
+  });
+ 
+});
+ 
+function resizePanel() {
+ 
+  width = $(window).width();
+  height = $(window).height();
+ 
+  mask_width = width * $('.item').length;
+ 
+  $('#debug').html(width + ' ' + height + ' ' + mask_width);
+ 
+  $('#wrapper, .item').css({
+    width: width,
+    height: height
+  });
+  $('#mask').css({
+    width: mask_width,
+    height: height
+  });
+  $('#wrapper').scrollTo($('a.selected').attr('href'), 0);
+ 
+}
